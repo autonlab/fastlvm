d6e627b7aeb94ed171a275c4f2b30f51581e8166
Jarod <jarod.d.wang@gmail.com>
2019-01-24 14:40:30 -0500

    add inputs as an argument

--- fastlvm/lda.py
+++ fastlvm/lda.py
@@ -183,10 +183,11 @@ class LDA(UnsupervisedLearnerPrimitiveBase[Inputs, Outputs, Params, HyperParams]
     def multi_produce(self, *, produce_methods: typing.Sequence[str], inputs: Inputs, timeout: float = None, iterations: int = None) -> base.MultiCallResult:
         pass
 
-    def fit_multi_produce(self, *, produce_methods: typing.Sequence[str], training_inputs: Inputs,
+    def fit_multi_produce(self, *, produce_methods: typing.Sequence[str], inputs: Inputs, training_inputs: Inputs,
                           validation_inputs: Inputs, vocabulary: VocabularyInputs,
                           timeout: float = None, iterations: int = None) -> base.MultiCallResult:  # type: ignore
         return self._fit_multi_produce(produce_methods=produce_methods, timeout=timeout, iterations=iterations,
+                                       inputs=inputs,
                                        training_inputs=training_inputs, validation_inputs=validation_inputs,
                                        vocabulary=vocabulary)
 
