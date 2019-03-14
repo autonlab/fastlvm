d15069977b51194278a1de0771fe4cd7b350ff9a
Donghan Wang <donghanw@cs.cmu.edu>
2019-03-14 12:34:43 -0400

    fix version: add patch version

--- fastlvm/lda.py
+++ fastlvm/lda.py
@@ -42,7 +42,7 @@ class LDA(UnsupervisedLearnerPrimitiveBase[Inputs, Outputs, Params, HyperParams]
 
     metadata = metadata_base.PrimitiveMetadata({
         "id": "f410b951-1cb6-481c-8d95-2d97b31d411d",
-        "version": "2.0",
+        "version": "2.0.0",
         "name": "Latent Dirichlet Allocation Topic Modelling",
         "description": "This class provides functionality for unsupervised inference on latent Dirichlet allocation, which is a probabilistic topic model of corpora of documents which seeks to represent the underlying thematic structure of the document collection. They have emerged as a powerful new technique of finding useful structure in an unstructured collection as it learns distributions over words. The high probability words in each distribution gives us a way of understanding the contents of the corpus at a very high level. In LDA, each document of the corpus is assumed to have a distribution over K topics, where the discrete topic distributions are drawn from a symmetric dirichlet distribution. Standard packages, like those in scikit learn are inefficient in addition to being limited to a single machine. Whereas our underlying C++ implementation can be distributed to run on multiple machines. To enable the distribution through python interface is work in progress. The API is similar to sklearn.decomposition.LatentDirichletAllocation.",
         "python_path": "d3m.primitives.natural_language_processing.lda.Fastlvm",
--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ utils_module = Extension('utilsc',
 )
 
 setup ( name = 'fastlvm',
-    version = '2.0',
+    version = '2.0.0',
     description = 'fastlvm -- fast search, clustering, and mixture modelling',
     keywords = 'd3m_primitive',
     cmdclass={'build_ext':build_ext},
